b45ab1b06ace7a09e430872accff8a576e57f824
BlueBoi904 <cliff.mirschel@gmail.com>
2022-08-19 03:05:22 -0700

    Adding solutions

---
+++ files/dynamic_programming/climb_stairs.py
@@ -0,0 +1,41 @@
+"""
+
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+"""
+
+
+def climbStairs(n):
+    def helper(num, memo=None):
+        if memo is None:
+            memo = {}
+        if num == 1:
+            return 1
+        if num == 2:
+            return 2
+
+        if num not in memo:
+            memo[num] = helper(num - 1) + helper(num - 2)
+        return memo[num]
+
+    return helper(n)
